526e975bc6bf86a9eb6d6970e8c4b5f67eefb5ca
malloudaya <malloudaya@gmail.com>
2024-01-12 16:32:30 -0800

    argc argv

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the name of the program
+ * @argc: int
+ * @argv: Array
+ * Return: 0
+ */
+
+
+int main(int argc, char const *argv[])
+{
+	(void)argc;
+	printf("%s\n", argv[0]);
+	return(0);
+}
