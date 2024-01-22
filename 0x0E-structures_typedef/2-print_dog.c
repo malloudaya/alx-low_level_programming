d9a500f7ce8b6445e423bc567ab9f098a66bd23a
malloudaya <malloudaya@gmail.com>
2024-01-22 14:56:13 -0800

    update

--- 0x0E-structures_typedef/2-print_dog.c
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -9,15 +9,13 @@ void print_dog(struct dog *d)
 	if  (d != NULL)
 	{
 		if (d->name == NULL)
-		{
 			printf("Name: (nil)\n");
-		}
-		if (d->owner == NULL)
-		{
-			printf("owner: (nil)\n");
-		}
-		printf("Name: %s\n", d->name);
+		else
+			printf("Name: %s\n", d->name);
 		printf("Age: %f\n", d->age);
-		printf("Owner: %s\n", d->owner);
+		if (d->owner == NULL)
+			printf("Owner: (nil)\n");
+		else
+			printf("Owner: %s\n", d->owner);
 	}
 }
