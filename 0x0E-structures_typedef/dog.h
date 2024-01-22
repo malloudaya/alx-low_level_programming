6d19a515360cb17328bcdf4ad4a2908a87236b7f
malloudaya <malloudaya@gmail.com>
2024-01-22 13:25:45 -0800

    update

---
+++ 0x0E-structures_typedef/dog.h
@@ -0,0 +1,17 @@
+#ifndef _DOG_H
+#define _DOG_H
+
+/**
+ * struct dog - defines a new type struct dog
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ */
+
+struct dog
+{
+	char *name;
+	float age;
+	char *owner;
+}
+#endif /* _DOG_H */
