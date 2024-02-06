907bed52b19213649379c5a21260b302f448648f
malloudaya <malloudaya@gmail.com>
2024-02-06 14:10:07 -0800

    update

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -8,13 +8,14 @@
  */
 size_t print_listint(const listint_t *h)
 {
-	size_t p = 0;
+	size_t nodes;
 
+	nodes = 0;
 	while (h != NULL)
 	{
 		printf("%d\n", h->n);
 		h = h->next;
-		p++;
+		nodes++;
 	}
-	return (p);
+	return (nodes);
 }
